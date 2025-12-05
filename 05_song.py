a0a312472e2021e2c65642b694ed84cb948edbfc
Londe <elion.rexhepi@hakju.at>
2025-12-05 09:28:37 +0100

    Songs

--- 05_song.py
+++ 05_song.py
@@ -35,21 +35,47 @@ Beispiel Ergebnis:
 """
 
 # TODO: Erstelle hier die Klasse Song
+class Song:
+    def __init__(self, titel,interpreten):
+        self.titel = titel
+        self.interpreten = interpreten
+        print(f"Neuer Song mit Titel {self.titel} &n Interpreten {self.interpreten} wurde erstellt!")
 
 
+    def zeige_info(self):
+        print(f"Neuer Song mit Titel {self.titel} &n Interpreten {self.interpreten} wurde erstellt!")
+
+    def interpret_hinzufügen(self,neuer_interpret):
+        self.interpreten.append(neuer_interpret)
+        print(f"{neuer_interpret} wurde hinzugefügt")
+        print(f"Alle Interpreten: {self.interpreten}")
+
+
+    def anzahl_interpreten(self):
+        return len(self.interpreten)
+    
+    def play(self):
+        print(f"▶️ Song '{self.titel}' wird gespielt...")
 # TODO: Erstelle einen Song mit einem Titel und 2 Interpreten deiner Wahl
-
+song_1 = Song(titel="Die with a smile", interpreten=["Bruno Mars", "Lady Gaga"])
+song_2 = Song(titel= "Zombie", interpreten=["Cranberris"])
+rock_song = Song(titel="My only Angel", interpreten=["Yungblud", "Aerosmith"])
 
 # TODO: Zeige die Song-Info
-
+song_1.zeige_info()
 
 # TODO: Füge einen weiteren Interpreten hinzu
-
-
-# TODO: Zeige die Anzahl der Interpreten
-
+song_1.interpret_hinzufügen("DJ Lenzi")
+song_1.interpret_hinzufügen("DJ Obdocha")
+# TODO: Zeige die Anzahl der Interpretens
+nr_artists = song_1.anzahl_interpreten()
+print(nr_artists)
+print(song_1.anzahl_interpreten())
 
 # TODO: Zeige die Song-Info erneut
-
+song_1.zeige_info()
 
 # TODO: Spiele den Song ab
+song_1.play()
+
+
